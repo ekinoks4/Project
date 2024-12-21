80871f9d02b01a04eed636adc7475e319383bd94
Merve Seren <mseren.datenna@gmail.com>
2024-12-21 21:15:18 +0000

    add documentation and tests

--- models/marts/company_profile/company_profile_page.sql
+++ models/marts/company_profile/company_profile_page.sql
@@ -6,10 +6,6 @@ select
     bid_amount,
     currency,
     case when bid_ranking = 1 then true else false end is_winner_bid,
-    count(*) over (partition by supplier) total_number_of_bids,
-    count(case when is_winner_bid then 1 else null end) over (
-        partition by supplier
-    ) total_number_of_winner_bids,
     buyer,
     type,
     source,
